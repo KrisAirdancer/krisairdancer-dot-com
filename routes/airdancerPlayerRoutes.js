d160c99473b43ae2ea21b4e3efc9fab10f8ae990
KrisAirdancer <chris.scott.marston@gmail.com>
2022-07-24 20:04:28 -0600

    Added homepage and changed routing to use routes files via Express.

---
+++ routes/airdancerPlayerRoutes.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const fs = require('fs');
+const mediaserver = require('mediaserver');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    console.log(`AT: /`);
+
+    generateXML();
+    res.render('airdancerPlayer');
+});
+
+router.get('/vip-playlist', (req, res) => {
+    console.log('AT: /vip-playlist');
+
+    generateXML();
+    res.render('vip_playlist.xml');
+})
+
+router.get('/track/:id', (req, res) => {
+    console.log(`AT: /track/:id`);
+
+    console.log(`:id: ${req.params.id}`);
+
+    let trackName = req.params.id.replaceAll('_', ' ');
+    console.log(`trackName: ${trackName}`);
+
+    mediaserver.pipe(req, res, `tracks/${trackName}`);
+    // res.render(`audio`, { title: "Auido File" });
+});
+
+/*******************
+ * OTHER FUNCTIONS *
+ *******************/
+
+function generateXML() {
+    console.log('AT: generateXML');
+
+    let data = '';
+
+    data += `
+        <playlist xmlns="https://xspf.org/ns/0/" version="1">
+            <trackList>
+    `;
+
+    let files = fs.readdirSync('tracks/');
+    files.forEach(fileName => {
+        fileName = fileName.replaceAll(' ', '_');
+
+        let trackName = fileName.substring(0, fileName.indexOf('.'));
+        trackName = trackName.replaceAll('_', ' ');
+
+        console.log('fileName:', fileName);
+        console.log('trackName:', trackName);
+
+        data += `
+                <track>
+                    <title>${trackName}</title>
+                    <location>http://localhost:8080/airdancer-player/track/${fileName}</location>
+                </track>
+        `;
+    });
+
+    data += `
+            </trackList>
+        </playlist>
+    `;
+
+    // console.log(data);
+
+    fs.writeFile('public/playlists/vip_playlist.xml', data, (err) => {
+        if (err) {
+            throw err;
+        };
+    });
+};
+
+/***********
+ * EXPORTS *
+ ***********/
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,41 @@
+const { application } = require('express');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const airdancerPlayerRoutes = require('./routes/airdancerPlayerRoutes');
+  
+// Static Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.static('public')); // This makes the directory 'public' and all of it's contents available to the frontend.
+  
+// View Engine Setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+  
+// TODO: Replace the redirect here to got to the krisairdancer.com directory page (homepage).
+app.get('/', (req, res) => {
+    console.log('AT: /');
+
+    res.redirect('/homepage');
+});
+
+app.get('/homepage', (req, res) => {
+    console.log(`AT: /homepage`);
+
+    res.render('homepage');
+});
+
+app.use('/airdancer-player', airdancerPlayerRoutes);
+
+app.use( (req, res) => { 
+    res.status(404).render('404', { title: '404'} );
+});
+
+app.listen(8080, (error) => {
+    if(error){
+        throw error;
+    }   
+    console.log("Server listening on port 8080");
+});
