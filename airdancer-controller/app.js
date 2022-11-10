d0b570a66c178715f329c223d9b0840910935f26
KrisAirdancer <chris.scott.marston@gmail.com>
2022-11-09 17:15:46 -0700

    Figuring things out...

--- airdancer-controller/app.js
+++ airdancer-controller/app.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const bodyParser = require("body-parser");
-// const axios = require('axios').default;
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 
 /***** CONFIGURATIONs *****/
 
-const PORT = 11001;
+const PORT = 12001;
 
 /***** APPLICATION SETUP *****/
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 /***** ROUTING *****/
 
@@ -20,37 +29,6 @@ app.get('/', (req, res) => {
     // console.log(req)
     res.send('Hello, World!')
 });
-
-app.get("/pw", (req, res) => {
-    console.log('/pw');
-    // console.log(`req:`);
-    // console.log(req.body);
-    // console.log(req.headers);
-    // res.sendStatus(200);
-  
-    axios.get("http://localhost:11002/string")
-         .then(response => {
-            console.log('***** BEGIN RESPONSE *****');
-            console.log(response.data);
-            console.log('***** END RESPONSE *****');
-         })
-         .catch(error => {
-            console.log('AXIOS ERROR at /string')
-            console.log(error);
-        });
-
-    res.sendStatus(200);
-    // res.write('Hallo');
-  });
-
-// app.get('/pw', (req, res) => {
-
-//     axios.get('localhost:11002/')
-//          .then(response => {
-//             console.log(response)
-//          })
-//     res.send('HERE');
-// });
 
 /***** LAUNCH APPLICATION *****/
 
--- personal-website/app.js
+++ personal-website/app.js
@@ -1,4 +1,6 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
 /***** CONFIGURATIONs *****/
 
@@ -8,11 +10,27 @@ const PORT = 11002;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
 /***** ROUTING *****/
 
-app.get('/string', (req, res) => {
+// Appends a string of text to the response body
+app.get('/body', (req, res) => {
     // res.write('Hello, from personal website.')
     res.send({ msg: 'Hello, from personal-web' });
+});
+
+app.get('/html-file', (req, res) => {
+    // res.send('<h1>TEST</h1>');
+    res.sendFile('./views/test.html', { root: __dirname });
+});
+
+app.get('/audio-file', (req, res) => {
+    res.sendFile('./origin.mp3', { root: __dirname });
+    // fs.createReadStream('origin.mp3').pipe(res); // Reads data from the origin.mp3 file and pipes it into the response
 });
 
 /***** LAUNCH APPLICATION *****/
