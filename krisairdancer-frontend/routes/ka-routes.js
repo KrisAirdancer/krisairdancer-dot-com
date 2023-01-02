8074ae99ed9e4a9e0690ae428a5b40afe33e8507
KrisAirdancer <chris.scott.marston@gmail.com>
2023-01-02 12:47:27 -0700

    Guestbook logic is complete.

--- krisairdancer-frontend/routes/ka-routes.js
+++ krisairdancer-frontend/routes/ka-routes.js
@@ -32,9 +32,26 @@ router.get('/sign-guestbook', (req, res) => {
         res.redirect('/404');
     }
 
+    // Code from here: https://stackoverflow.com/questions/10211145/getting-current-date-and-time-in-javascript
+    // Generates a string containing today's date
+    Date.prototype.today = function () { 
+        // return ((this.getDate() < 10)?"0":"") + this.getDate() +"/"+(((this.getMonth()+1) < 10)?"0":"") + (this.getMonth()+1) +"/"+ this.getFullYear();
+        return (((this.getUTCMonth()+1) < 10)?"0":"") + (this.getUTCMonth()+1) + "/" +((this.getUTCDate() < 10)?"0":"") + this.getUTCDate() + "/" + this.getUTCFullYear();
+    }
+
+    // Generates a string containing the current time
+    Date.prototype.timeNow = function () {
+        return ((this.getUTCHours() < 10)?"0":"") + this.getUTCHours() +":"+ ((this.getUTCMinutes() < 10)?"0":"") + this.getUTCMinutes() +":"+ ((this.getUTCSeconds() < 10)?"0":"") + this.getUTCSeconds();
+    }
+
+    
+    let entryDate = new Date();
+
     let guestbookEntry = {
+        favWebsite: `${req.query.favWebsite}`,
         handle: `${req.query.handle}`,
-        message: `${req.query.message}`
+        message: `${req.query.message}`,
+        timestamp: `${entryDate.today()} ${entryDate.timeNow()}`
     };
 
     guestbook.push(guestbookEntry);
--- krisairdancer-frontend/views/ka-views/guestbook.js
+++
@@ -1,8 +0,0 @@
-// window.onload = () => {
-
-    console.log('It works!');
-
-    let guestbookEntries = document.querySelector('guestbook-entries');
-
-    guestbookEntries.textContent = 'bricks!';
-// }
