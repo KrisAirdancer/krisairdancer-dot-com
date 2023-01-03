96c062de722dcb8392a64e7084ad863221e426d3
KrisAirdancer <chris.scott.marston@gmail.com>
2023-01-03 09:15:32 -0700

    Added CS 3390 Group Nums project to site.

--- krisairdancer-frontend/routes/portfolio-routes.js
+++ krisairdancer-frontend/routes/portfolio-routes.js
@@ -7,5 +7,9 @@ router.get('/', (req, res) => {
     res.render('portfolio-views/portfolio');
 });
 
+router.get('/3390-group-nums', (req, res) => {
+    res.render('portfolio-views/3390-group-nums');
+});
+
 // TODO: Read up on why we need to export the router object from the "routes" files.
 module.exports = router;
