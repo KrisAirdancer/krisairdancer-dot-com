8b1c5d667b0f44b1c47a3211164c1a821e4f0adb
krisairdancer <chris.scott.marston@gmail.com>
2024-03-12 15:10:50 -0600

    Restyled terminal banner.

--- frontend/src/components/terminal/TerminalBanner.jsx
+++ frontend/src/components/terminal/TerminalBanner.jsx
@@ -5,12 +5,11 @@ export default function TerminalBanner()
     // TODO: Move this to a different location. A config file or something?
     let siteVersion = "2.0.1"
 
-    let banner = "  ██████╗███╗   ███╗ ╦╔═╦═╗╦╔═╗╔═╗╦╦═╗╔╦╗╔═╗╔╗╔╔═╗╔═╗╦═╗\n" +
-                " ██╔════╝████╗ ████║ ╠╩╗╠╦╝║╚═╗╠═╣║╠╦╝ ║║╠═╣║║║║  ║╣ ╠╦╝\n" +
-                " ██║     ██╔████╔██║ ╩ ╩╩╚═╩╚═╝╩ ╩╩╩╚══╩╝╩ ╩╝╚╝╚═╝╚═╝╩╚═\n" +
-                " ██║     ██║╚██╔╝██║ The Portfolio of Chris Marston\n" +
-                ` ╚██████╗██║ ╚═╝ ██║ v${siteVersion}\n` +
-                "  ╚═════╝╚═╝     ╚═╝"
+    let banner = "╦╔═╦═╗╦╔═╗╔═╗╦╦═╗╔╦╗╔═╗╔╗╔╔═╗╔═╗╦═╗\n" +
+                 "╠╩╗╠╦╝║╚═╗╠═╣║╠╦╝ ║║╠═╣║║║║  ║╣ ╠╦╝\n" +
+                 "╩ ╩╩╚═╩╚═╝╩ ╩╩╩╚══╩╝╩ ╩╝╚╝╚═╝╚═╝╩╚═\n" +
+                 "The Portfolio of Chris Marston\n" +
+                 `v${siteVersion}\n`
 
     return (
         <div id='terminal-banner'>
