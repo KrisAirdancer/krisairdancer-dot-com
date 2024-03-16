0ed0f9b4ac98a9a6b95ffe964a425446dacb2148
krisairdancer <chris.scott.marston@gmail.com>
2024-03-15 20:15:06 -0600

    Added event handler to respond to the 'Enter' key being pressed.

--- frontend/src/components/terminal/TerminalInput.jsx
+++ frontend/src/components/terminal/TerminalInput.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react'
 import './TerminalInput.css'
 import TerminalLeader from "./TerminalLeader"
 
 export default function TerminalInput()
 {
+    let [ userInput, setUserInput ] = useState("help")
+
+    function handleKeyPress(event)
+    {
+        if (event.key == "Enter")
+        {
+            console.log("ENTER")
+            console.log("INPUT: ", userInput)
+        }
+
+        // TODO: I'm going to have to figure out how I want to manage the state of the terminal.
+        // > I'll need to append the appropriate component to the UI with the appropriate state (text) depending on what text is passed in.
+    }
+
     return (
         <>
             <TerminalLeader />
             <div>
                 <span id="input-symbol">&gt;</span>
-                <input type="text" />
+                <input type="text"
+                    value={userInput}
+                    onChange={event => setUserInput(event.target.value)}
+                    onKeyDown={handleKeyPress}
+                />
             </div>
         </>
     )
